3081950496bd95dc88d54ee8a81b9f0c8c6c52c6
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-18 13:21:38 +0900

    Import roles

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,8 +1,3 @@
-resource "aws_iam_role" "dynamodb_task_role" {
-  name               = "sock-shop-DynamoDbTaskRole-13YK50YRC8S9F"
-  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume_role_policy.json
-}
-
 data "aws_iam_policy_document" "ecs_tasks_assume_role_policy" {
   version = "2008-10-17"
   statement {
@@ -15,9 +10,24 @@ data "aws_iam_policy_document" "ecs_tasks_assume_role_policy" {
   }
 }
 
+resource "aws_iam_role" "dynamodb_task_role" {
+  name               = "sock-shop-DynamoDbTaskRole-13YK50YRC8S9F"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume_role_policy.json
+}
+
 resource "aws_iam_role_policy_attachment" "dynamodb_task_role-amazon_dynamodb_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
   role       = aws_iam_role.dynamodb_task_role.name
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "sock-shop-EcsTaskExecutionRole-4VE06B3BDH02"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role-amazon_ecs_task_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
 }
 
 resource "aws_service_discovery_private_dns_namespace" "local" {
