b7b5bc32317d0f9b3c61f36d8714d756c0e41977
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-19 01:49:19 +0900

    Import carts-db ecs service

--- terraform/carts.tf
+++ terraform/carts.tf
@@ -1,3 +1,24 @@
+resource "aws_ecs_service" "carts_db" {
+  name          = "sock-shop-CartsDBService-mPiXfiIT9Aky"
+  desired_count = 1
+  launch_type   = "FARGATE"
+  cluster       = aws_ecs_cluster.sock_shop.id
+  task_definition = "${aws_ecs_task_definition.carts_db.id}:${aws_ecs_task_definition.carts_db.revision}"
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups = [aws_security_group.rds_importer_allowed_ports.id]
+    subnets = [
+      aws_subnet.public_subnet_1.id,
+      aws_subnet.public_subnet_2.id,
+    ]
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.carts_db.arn
+  }
+}
+
 resource "aws_ecs_task_definition" "carts_db" {
   container_definitions = jsonencode([
     {
