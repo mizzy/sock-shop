9f16931c9f24a2a3212099c37859d51b0a4e4483
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-18 12:59:37 +0900

    Import IAM role

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,20 @@
+resource "aws_iam_role" "dynamodb_task_role" {
+  name               = "sock-shop-DynamoDbTaskRole-13YK50YRC8S9F"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ecs_tasks_assume_role_policy" {
+  version = "2008-10-17"
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["ecs-tasks.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
 resource "aws_service_discovery_private_dns_namespace" "local" {
   name = "local"
   vpc  = aws_vpc.sock_shop.id
