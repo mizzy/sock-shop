64fb82889ee1d6df9252d7b36d9b09b99995d8d3
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-18 19:39:45 +0900

    commit

--- terraform/rds_importer.tf
+++ terraform/rds_importer.tf
@@ -5,3 +5,30 @@ resource "aws_security_group" "rds_importer_allowed_ports" {
     "Name" = "rds-importer-allowed-ports"
   }
 }
+
+resource "aws_security_group_rule" "rds_importer_allowed_port_ssh" {
+  type              = "ingress"
+  security_group_id = aws_security_group.rds_importer_allowed_ports.id
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "rds_importer_allowed_from_elb" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.rds_importer_allowed_ports.id
+  protocol                 = -1
+  from_port                = 0
+  to_port                  = 65536
+  source_security_group_id = aws_security_group.elb_allowed_ports.id
+}
+
+resource "aws_security_group_rule" "rds_importer_allowed_from_self" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.rds_importer_allowed_ports.id
+  protocol                 = -1
+  from_port                = 0
+  to_port                  = 65536
+  source_security_group_id = aws_security_group.rds_importer_allowed_ports.id
+}
