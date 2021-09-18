2244ae9006278e2710b08ef425c8d9dba1e10776
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-18 12:52:52 +0900

    commit

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,8 @@
 resource "aws_vpc" "sock_shop" {
   cidr_block = "172.31.0.0/16"
+  tags = {
+    "Name" = "sock shop"
+  }
 }
 
 resource "aws_vpc_dhcp_options" "local" {
@@ -10,4 +13,20 @@ resource "aws_vpc_dhcp_options" "local" {
 resource "aws_vpc_dhcp_options_association" "local" {
   vpc_id          = aws_vpc.sock_shop.id
   dhcp_options_id = aws_vpc_dhcp_options.local.id
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  cidr_block = "172.31.0.0/24"
+  vpc_id     = aws_vpc.sock_shop.id
+  tags = {
+    "Name" = "public subnet 1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  cidr_block = "172.31.1.0/24"
+  vpc_id     = aws_vpc.sock_shop.id
+  tags = {
+    "Name" = "public subnet 2"
+  }
 }
