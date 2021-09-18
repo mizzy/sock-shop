b1451ed7631d6ae7f64bd935c04c2898b9d5c59a
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-18 20:59:54 +0900

    Import instance

--- terraform/rds_importer.tf
+++ terraform/rds_importer.tf
@@ -32,3 +32,28 @@ resource "aws_security_group_rule" "rds_importer_allowed_from_self" {
   to_port                  = 65536
   source_security_group_id = aws_security_group.rds_importer_allowed_ports.id
 }
+
+resource "aws_instance" "rds_importer" {
+  ami                         = "ami-56d4ad31"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.rds_importer_allowed_ports.id]
+  subnet_id                   = aws_subnet.public_subnet_1.id
+  key_name                    = "sock-shop-cloudformation"
+
+  user_data = <<EOF
+#!/bin/bash -xe
+wget https://raw.githubusercontent.com/microservices-demo/catalogue/master/docker/catalogue-db/data/dump.sql
+mysql -u catalogue_user --password=default_password -h ${aws_db_instance.catalogue.address} \
+  -f -D socksdb < dump.sql
+EOF
+
+  tags = {
+    "Name" = "RDS Importer - sock-shop"
+  }
+
+  # Temporary ignore user_data because it's different from the original cloudformation template
+  lifecycle {
+    ignore_changes = [user_data]
+  }
+}
