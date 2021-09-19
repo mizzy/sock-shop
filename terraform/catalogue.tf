e45cd74a0b9547c32c34e22242a9e4c961a5daf8
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-19 11:48:24 +0900

    Import catalogue task def

--- terraform/catalogue.tf
+++ terraform/catalogue.tf
@@ -33,3 +33,61 @@ resource "aws_db_instance" "catalogue" {
   username = "catalogue_user"
   password = "default_password"
 }
+
+resource "aws_ecs_task_definition" "catalogue" {
+  container_definitions = jsonencode([
+    {
+      name = "catalogue",
+      environment = [
+        {
+          name  = "ZIPKIN",
+          value = "http://zipkin:9411/api/v1/spans",
+        },
+      ],
+      command = [
+        "/app",
+        "-port=80",
+        "-DSN=catalogue_user:default_password@tcp(${aws_db_instance.catalogue.address}:${aws_db_instance.catalogue.port})/socksdb",
+      ],
+      cpu                   = 0,
+      dnsSearchDomains      = [],
+      dnsServers            = [],
+      dockerLabels          = {},
+      dockerSecurityOptions = [],
+      entryPoint            = [],
+      environmentFiles      = [],
+      extraHosts            = [],
+      links                 = [],
+      mountPoints           = [],
+      secrets               = [],
+      systemControls        = [],
+      ulimits               = [],
+      volumesFrom           = [],
+      essential             = true,
+      image                 = "weaveworksdemos/catalogue:0.3.5",
+      logConfiguration = {
+        secretOptions = [],
+        logDriver     = "awslogs",
+        options = {
+          awslogs-group         = "sock-shop",
+          awslogs-region        = data.aws_region.current.name,
+          awslogs-stream-prefix = "catalogue",
+        }
+      },
+      portMappings = [
+        {
+          containerPort = 80,
+          hostPort      = 80,
+          protocol      = "tcp",
+        }
+      ],
+    }
+  ])
+
+  cpu    = 256
+  memory = 512
+
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  family                   = "sock-shop-CatalogueTask-DfaZsRAivWhG"
+  requires_compatibilities = ["FARGATE"]
+}
