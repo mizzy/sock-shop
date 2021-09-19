c2cca021a6e9dd6d38c872cae1ffd314f75780c9
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-19 14:41:32 +0900

    Use module

--- terraform/carts_db.tf
+++ terraform/carts_db.tf
@@ -1,85 +1,24 @@
-resource "aws_ecs_service" "carts_db" {
-  name            = "sock-shop-CartsDBService-mPiXfiIT9Aky"
-  desired_count   = 1
-  launch_type     = "FARGATE"
-  cluster         = aws_ecs_cluster.sock_shop.id
-  task_definition = "${aws_ecs_task_definition.carts_db.id}:${aws_ecs_task_definition.carts_db.revision}"
+module "ecs_carts_db" {
+  source = "./modules/ecs"
 
-  network_configuration {
-    assign_public_ip = true
-    security_groups  = [aws_security_group.ecs.id]
-    subnets = [
-      aws_subnet.public_subnet_1.id,
-      aws_subnet.public_subnet_2.id,
-    ]
+  service = {
+    name               = "sock-shop-CartsDBService-mPiXfiIT9Aky"
+    cluster_id         = aws_ecs_cluster.sock_shop.id
+    security_group_ids = [aws_security_group.ecs.id]
+    subnet_ids         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   }
 
-  service_registries {
-    registry_arn = aws_service_discovery_service.carts_db.arn
+  task = {
+    name               = "carts-db"
+    image              = "mongo"
+    family             = "sock-shop-CartsDBTask-6LKqO3WQLkdN"
+    execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+    port               = 27017
+    memory             = 512
   }
-}
 
-resource "aws_ecs_task_definition" "carts_db" {
-  container_definitions = jsonencode([
-    {
-      name                  = "carts-db",
-      command               = [],
-      cpu                   = 0,
-      dnsSearchDomains      = [],
-      dnsServers            = [],
-      dockerLabels          = {},
-      dockerSecurityOptions = [],
-      entryPoint            = [],
-      environment           = [],
-      environmentFiles      = [],
-      extraHosts            = [],
-      links                 = [],
-      mountPoints           = [],
-      secrets               = [],
-      systemControls        = [],
-      ulimits               = [],
-      volumesFrom           = [],
-      essential             = true,
-      image                 = "mongo",
-      logConfiguration = {
-        secretOptions = [],
-        logDriver     = "awslogs",
-        options = {
-          awslogs-group         = "sock-shop",
-          awslogs-region        = data.aws_region.current.name,
-          awslogs-stream-prefix = "carts-db",
-        }
-      },
-      portMappings = [
-        {
-          containerPort = 27017,
-          hostPort      = 27017,
-          protocol      = "tcp",
-        }
-      ],
-    }
-  ])
-
-  cpu    = 256
-  memory = 512
-
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  family                   = "sock-shop-CartsDBTask-6LKqO3WQLkdN"
-  requires_compatibilities = ["FARGATE"]
-}
-
-resource "aws_service_discovery_service" "carts_db" {
-  name = "carts-db"
-
-  dns_config {
+  registry = {
+    name         = "carts-db"
     namespace_id = aws_service_discovery_private_dns_namespace.local.id
-    dns_records {
-      ttl  = 10
-      type = "A"
-    }
-  }
-
-  health_check_custom_config {
-    failure_threshold = 1
   }
 }
