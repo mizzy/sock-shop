1a15a9e9496ca4954c5705a0c883d44614f69101
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-19 12:12:20 +0900

    Import frontend target group

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,9 +1,16 @@
 resource "aws_lb" "frontend" {
-  name = "sockshop"
+  name         = "sockshop"
   idle_timeout = 30
   subnets = [
     aws_subnet.public_subnet_1.id,
     aws_subnet.public_subnet_2.id,
   ]
   security_groups = [aws_security_group.elb_allowed_ports.id]
+}
+
+resource "aws_lb_target_group" "frontend" {
+  vpc_id      = aws_vpc.sock_shop.id
+  target_type = "ip"
+  protocol    = "HTTP"
+  port        = 8079
 }
