43f90215abe2955d34f42f17530e36b4a400dd17
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-20 11:12:37 +0900

    Import zipkin service listener and listener rule

--- terraform/zipkin_service.tf
+++ terraform/zipkin_service.tf
@@ -10,3 +10,25 @@ resource "aws_lb_target_group" "zipkin_service" {
     unhealthy_threshold = 2
   }
 }
+
+resource "aws_lb_listener" "zipkin_service" {
+  load_balancer_arn = aws_lb.lb.arn
+  port = 9411
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.zipkin_service.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "zipkin_service" {
+  listener_arn = aws_lb_listener.zipkin_service.arn
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.zipkin_service.arn
+  }
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
