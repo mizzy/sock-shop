5e470c93de94594d78b8bfbd6c40810ac7a7193e
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-21 21:44:48 +0900

    Update code

--- pulumi/lib/ecs/ecs.go
+++ pulumi/lib/ecs/ecs.go
@@ -3,14 +3,17 @@ package ecs
 import "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 
 func NewEcs(ctx *pulumi.Context) error {
-	err := cloudWatchLogs(ctx)
-	if err != nil {
-		return err
+	resources := []func(ctx *pulumi.Context) error{
+		cloudWatchLogs,
+		dynamoDbTaskRole,
 	}
 
-	err = dynamoDbTaskRole(ctx)
-	if err != nil {
-		return err
+	for _, r := range resources {
+		err := r(ctx)
+		if err != nil {
+			return err
+		}
+
 	}
 
 	return nil
