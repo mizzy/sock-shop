8a37aeaacf51e0bc226db9ec431b8ffce7622f42
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-23 08:59:38 +0900

    Stop instances

--- scripts/stop.rb
+++ scripts/stop.rb
@@ -2,11 +2,12 @@
 
 require 'aws-sdk-rds'
 require 'aws-sdk-ecs'
+require 'aws-sdk-ec2'
 
 rds = Aws::RDS::Client.new
 
 rds.describe_db_instances.db_instances.each do |db|
-  # resp = rds.stop_db_instance(db_instance_identifier: db.db_instance_identifier)
+  rds.stop_db_instance(db_instance_identifier: db.db_instance_identifier)
 end
 
 ecs = Aws::ECS::Client.new
@@ -15,4 +16,11 @@ ecs.list_tasks(cluster: 'Sock-Shop').task_arns.each do |task|
     cluster: 'Sock-Shop',
     task: task,
   })
+end
+
+ec2 = Aws::EC2::Client.new
+ec2.describe_instances.reservations.each do |r|
+  r.instances.each do |i|
+    ec2.stop_instances({ instance_ids: [i.instance_id] })
+  end
 end
