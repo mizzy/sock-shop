89ad2ec1f22282bd6268f5fbb022c749cc125c6a
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-22 14:59:55 +0900

    Import VPC DHCP Options

--- pulumi/lib/vpc/vpc.go
+++ pulumi/lib/vpc/vpc.go
@@ -6,6 +6,23 @@ import (
 )
 
 func NewVpc(ctx *pulumi.Context) error {
+	resources := []func(ctx *pulumi.Context) error{
+		vpc,
+	}
+
+	for _, r := range resources {
+		err := r(ctx)
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+
+}
+
+func vpc(ctx *pulumi.Context) error {
 	_, err := ec2.NewVpc(ctx, "sock-shop", &ec2.VpcArgs{
 		AssignGeneratedIpv6CidrBlock: pulumi.Bool(false),
 		CidrBlock:                    pulumi.String("172.31.0.0/16"),
@@ -14,6 +31,16 @@ func NewVpc(ctx *pulumi.Context) error {
 		InstanceTenancy:              pulumi.String("default"),
 		Tags: pulumi.StringMap{
 			"Name": pulumi.String("sock-shop"),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = ec2.NewVpcDhcpOptions(ctx, "local", &ec2.VpcDhcpOptionsArgs{
+		DomainName: pulumi.String("local"),
+		DomainNameServers: pulumi.StringArray{
+			pulumi.String("AmazonProvidedDNS"),
 		},
 	})
 	if err != nil {
