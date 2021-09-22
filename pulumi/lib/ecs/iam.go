c088f5e38392732c55852f2addc7ed627e6bd4e1
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-22 11:28:39 +0900

    Import IanRolePolicyAttachment

--- pulumi/lib/ecs/iam.go
+++ pulumi/lib/ecs/iam.go
@@ -6,12 +6,20 @@ import (
 )
 
 func dynamoDbTaskRole(ctx *pulumi.Context) error {
-	_, err := iam.NewRole(ctx, "sock-shop-DynamoDbTaskRole", &iam.RoleArgs{
+	role, err := iam.NewRole(ctx, "sock-shop-DynamoDbTaskRole", &iam.RoleArgs{
 		AssumeRolePolicy:    pulumi.Any("{\"Version\":\"2008-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"ecs-tasks.amazonaws.com\"},\"Action\":\"sts:AssumeRole\"}]}"),
 		ForceDetachPolicies: pulumi.Bool(false),
 		MaxSessionDuration:  pulumi.Int(3600),
 		Name:                pulumi.String("sock-shop-DynamoDbTaskRole-13YK50YRC8S9F"),
 		Path:                pulumi.String("/"),
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = iam.NewRolePolicyAttachment(ctx, "dynamodb_task_role-amazon_dynamodb_full_access", &iam.RolePolicyAttachmentArgs{
+		PolicyArn: pulumi.String("arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"),
+		Role:      pulumi.Any(role.Name),
 	})
 	if err != nil {
 		return err
