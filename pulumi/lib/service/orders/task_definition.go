9622a6adde2127c065ec7e9071e2259909b6c419
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-24 23:17:46 +0900

    Import orders task def

--- pulumi/lib/service/orders/task_definition.go
+++ pulumi/lib/service/orders/task_definition.go
@@ -1,6 +1,7 @@
 package orders
 
 import (
+	e "github.com/mizzy/sock-shop/pulumi/lib/ecs"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/ecs"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -8,24 +9,22 @@ import (
 var taskDef *ecs.TaskDefinition
 
 func newTaskDefinition(ctx *pulumi.Context) error {
-	/*
-		var err error
-		taskDef, err = ecs.NewTaskDefinition(ctx, "carts", &ecs.TaskDefinitionArgs{
-			ContainerDefinitions: pulumi.String("[{\"command\":[],\"cpu\":0,\"dnsSearchDomains\":[],\"dnsServers\":[],\"dockerLabels\":{},\"dockerSecurityOptions\":[],\"entryPoint\":[],\"environment\":[],\"environmentFiles\":[],\"essential\":true,\"extraHosts\":[],\"image\":\"weaveworksdemos/carts:0.4.8\",\"links\":[],\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-group\":\"sock-shop\",\"awslogs-region\":\"ap-northeast-1\",\"awslogs-stream-prefix\":\"carts\"},\"secretOptions\":[]},\"mountPoints\":[],\"name\":\"carts\",\"portMappings\":[{\"containerPort\":80,\"hostPort\":80,\"protocol\":\"tcp\"}],\"secrets\":[],\"systemControls\":[],\"ulimits\":[],\"volumesFrom\":[]}]"),
-			Cpu:                  pulumi.String("256"),
-			ExecutionRoleArn:     e.TaskExecutionRole.Arn,
-			Family:               pulumi.String("sock-shop-CartsTask-eIq5v1xKpl13"),
-			Memory:               pulumi.String("1024"),
-			RequiresCompatibilities: pulumi.StringArray{
-				pulumi.String("FARGATE"),
-			},
-			NetworkMode: pulumi.String("awsvpc"),
-		})
-		if err != nil {
-			return err
-		}
+	_, err := ecs.NewTaskDefinition(ctx, "orders", &ecs.TaskDefinitionArgs{
+		ContainerDefinitions: pulumi.String("[{\"command\":[],\"cpu\":0,\"dnsSearchDomains\":[],\"dnsServers\":[],\"dockerLabels\":{},\"dockerSecurityOptions\":[],\"entryPoint\":[],\"environment\":[{\"name\":\"AWS_DYNAMODB_ENDPOINT\",\"value\":\"dynamodb.ap-northeast-1.amazonaws.com\"}],\"environmentFiles\":[],\"essential\":true,\"extraHosts\":[],\"image\":\"weaveworksdemos/orders-aws\",\"links\":[],\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-group\":\"sock-shop\",\"awslogs-region\":\"ap-northeast-1\",\"awslogs-stream-prefix\":\"orders\"},\"secretOptions\":[]},\"mountPoints\":[],\"name\":\"orders\",\"portMappings\":[{\"containerPort\":80,\"hostPort\":80,\"protocol\":\"tcp\"}],\"secrets\":[],\"systemControls\":[],\"ulimits\":[],\"volumesFrom\":[]}]"),
+		Cpu:                  pulumi.String("256"),
+		ExecutionRoleArn:     e.TaskExecutionRole.Arn,
+		Family:               pulumi.String("sock-shop-OrdersTask-XTi9XoWDVfLy"),
+		Memory:               pulumi.String("1024"),
+		RequiresCompatibilities: pulumi.StringArray{
+			pulumi.String("FARGATE"),
+		},
+		TaskRoleArn: taskRole.Arn,
+		NetworkMode: pulumi.String("awsvpc"),
+	})
 
-	*/
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
