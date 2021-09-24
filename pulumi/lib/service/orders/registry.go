22d96183509ccb083a2f7396411e121137952811
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-24 23:27:40 +0900

    Import orders task def

--- pulumi/lib/service/orders/registry.go
+++ pulumi/lib/service/orders/registry.go
@@ -1,6 +1,7 @@
 package orders
 
 import (
+	e "github.com/mizzy/sock-shop/pulumi/lib/ecs"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/servicediscovery"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -8,27 +9,27 @@ import (
 var registry *servicediscovery.Service
 
 func newRegistry(ctx *pulumi.Context) error {
-	/*
-		var err error
-		registry, err = servicediscovery.NewService(ctx, "carts", &servicediscovery.ServiceArgs{
-			DnsConfig: &servicediscovery.ServiceDnsConfigArgs{
-				DnsRecords: &servicediscovery.ServiceDnsConfigDnsRecordArray{
-					&servicediscovery.ServiceDnsConfigDnsRecordArgs{
-						Ttl:  pulumi.Int(10),
-						Type: pulumi.String("A"),
-					},
+	var err error
+
+	registry, err = servicediscovery.NewService(ctx, "orders", &servicediscovery.ServiceArgs{
+		DnsConfig: &servicediscovery.ServiceDnsConfigArgs{
+			DnsRecords: &servicediscovery.ServiceDnsConfigDnsRecordArray{
+				&servicediscovery.ServiceDnsConfigDnsRecordArgs{
+					Ttl:  pulumi.Int(10),
+					Type: pulumi.String("A"),
 				},
-				NamespaceId: ecs.PrivateDnsNamespace.ID(),
 			},
-			HealthCheckCustomConfig: &servicediscovery.ServiceHealthCheckCustomConfigArgs{
-				FailureThreshold: pulumi.Int(1),
-			},
-			Name: pulumi.String("carts"),
-		})
-		if err != nil {
-			return err
-		}
-	*/
+			NamespaceId: e.PrivateDnsNamespace.ID(),
+		},
+		HealthCheckCustomConfig: &servicediscovery.ServiceHealthCheckCustomConfigArgs{
+			FailureThreshold: pulumi.Int(1),
+		},
+		Name: pulumi.String("orders"),
+	})
+
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
