383aec1272a8d390dab7ea104a7e35e1497f46fd
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-24 10:53:27 +0900

    Import carts service

--- pulumi/lib/service/carts/carts.go
+++ pulumi/lib/service/carts/carts.go
@@ -6,6 +6,7 @@ func NewCarts(ctx *pulumi.Context) error {
 	resources := []func(ctx2 *pulumi.Context) error{
 		newTaskDefinition,
 		newRegistry,
+		newService,
 	}
 
 	for _, r := range resources {
--- pulumi/lib/service/carts/task_definition.go
+++ pulumi/lib/service/carts/task_definition.go
@@ -6,8 +6,11 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
+var taskDef *ecs.TaskDefinition
+
 func newTaskDefinition(ctx *pulumi.Context) error {
-	_, err := ecs.NewTaskDefinition(ctx, "carts", &ecs.TaskDefinitionArgs{
+	var err error
+	taskDef, err = ecs.NewTaskDefinition(ctx, "carts", &ecs.TaskDefinitionArgs{
 		ContainerDefinitions: pulumi.String("[{\"command\":[],\"cpu\":0,\"dnsSearchDomains\":[],\"dnsServers\":[],\"dockerLabels\":{},\"dockerSecurityOptions\":[],\"entryPoint\":[],\"environment\":[],\"environmentFiles\":[],\"essential\":true,\"extraHosts\":[],\"image\":\"weaveworksdemos/carts:0.4.8\",\"links\":[],\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-group\":\"sock-shop\",\"awslogs-region\":\"ap-northeast-1\",\"awslogs-stream-prefix\":\"carts\"},\"secretOptions\":[]},\"mountPoints\":[],\"name\":\"carts\",\"portMappings\":[{\"containerPort\":80,\"hostPort\":80,\"protocol\":\"tcp\"}],\"secrets\":[],\"systemControls\":[],\"ulimits\":[],\"volumesFrom\":[]}]"),
 		Cpu:                  pulumi.String("256"),
 		ExecutionRoleArn:     e.TaskExecutionRole.Arn,
