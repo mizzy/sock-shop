591922755a2a68cfeadbd763f7e833c207018fb8
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-25 21:33:04 +0900

    Import zipkin task def

--- pulumi/lib/service/zipkin/task_definition.go
+++ pulumi/lib/service/zipkin/task_definition.go
@@ -1,6 +1,7 @@
 package zipkin
 
 import (
+	e "github.com/mizzy/sock-shop/pulumi/lib/ecs"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/ecs"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -8,23 +9,21 @@ import (
 var taskDef *ecs.TaskDefinition
 
 func newTaskDefinition(ctx *pulumi.Context) error {
-	/*
-		var err error
-		taskDef, err = ecs.NewTaskDefinition(ctx, "carts", &ecs.TaskDefinitionArgs{
-			ContainerDefinitions: pulumi.String("[{\"command\":[],\"cpu\":0,\"dnsSearchDomains\":[],\"dnsServers\":[],\"dockerLabels\":{},\"dockerSecurityOptions\":[],\"entryPoint\":[],\"environment\":[],\"environmentFiles\":[],\"essential\":true,\"extraHosts\":[],\"image\":\"weaveworksdemos/carts:0.4.8\",\"links\":[],\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-group\":\"sock-shop\",\"awslogs-region\":\"ap-northeast-1\",\"awslogs-stream-prefix\":\"carts\"},\"secretOptions\":[]},\"mountPoints\":[],\"name\":\"carts\",\"portMappings\":[{\"containerPort\":80,\"hostPort\":80,\"protocol\":\"tcp\"}],\"secrets\":[],\"systemControls\":[],\"ulimits\":[],\"volumesFrom\":[]}]"),
-			Cpu:                  pulumi.String("256"),
-			ExecutionRoleArn:     e.TaskExecutionRole.Arn,
-			Family:               pulumi.String("sock-shop-CartsTask-eIq5v1xKpl13"),
-			Memory:               pulumi.String("1024"),
-			RequiresCompatibilities: pulumi.StringArray{
-				pulumi.String("FARGATE"),
-			},
-			NetworkMode: pulumi.String("awsvpc"),
-		})
-		if err != nil {
-			return err
-		}
-	*/
+	var err error
+	taskDef, err = ecs.NewTaskDefinition(ctx, "zipkin", &ecs.TaskDefinitionArgs{
+		ContainerDefinitions: pulumi.String("[{\"command\":[],\"cpu\":0,\"dnsSearchDomains\":[],\"dnsServers\":[],\"dockerLabels\":{},\"dockerSecurityOptions\":[],\"entryPoint\":[],\"environment\":[{\"name\":\"MYSQL_HOST\",\"value\":\"zipkin-mysql\"},{\"name\":\"MYSQL_PASS\",\"value\":\"zipkin\"},{\"name\":\"MYSQL_USER\",\"value\":\"zipkin\"},{\"name\":\"STORAGE_TYPE\",\"value\":\"mysql\"}],\"environmentFiles\":[],\"essential\":true,\"extraHosts\":[],\"image\":\"openzipkin/zipkin\",\"links\":[],\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-group\":\"sock-shop\",\"awslogs-region\":\"ap-northeast-1\",\"awslogs-stream-prefix\":\"zipkin\"},\"secretOptions\":[]},\"mountPoints\":[],\"name\":\"zipkin\",\"portMappings\":[{\"containerPort\":9411,\"hostPort\":9411,\"protocol\":\"tcp\"}],\"secrets\":[],\"systemControls\":[],\"ulimits\":[],\"volumesFrom\":[]}]"),
+		Cpu:                  pulumi.String("512"),
+		ExecutionRoleArn:     e.TaskExecutionRole.Arn,
+		Family:               pulumi.String("sock-shop-ZipkinTask-xR5HsG5Dz7Qr"),
+		Memory:               pulumi.String("1024"),
+		RequiresCompatibilities: pulumi.StringArray{
+			pulumi.String("FARGATE"),
+		},
+		NetworkMode: pulumi.String("awsvpc"),
+	})
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
