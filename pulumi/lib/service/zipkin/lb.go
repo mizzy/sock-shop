00087f7bc4cc30171400db707058d452047dec23
Gosuke Miyashita <gosukenator@gmail.com>
2021-09-25 21:27:59 +0900

    Import listener and listener rule

--- pulumi/lib/service/zipkin/lb.go
+++ pulumi/lib/service/zipkin/lb.go
@@ -1,6 +1,7 @@
 package zipkin
 
 import (
+	"github.com/mizzy/sock-shop/pulumi/lib/ecs"
 	"github.com/mizzy/sock-shop/pulumi/lib/vpc"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/alb"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -9,7 +10,8 @@ import (
 var targetGroup *alb.TargetGroup
 
 func newLb(ctx *pulumi.Context) error {
-	_, err := alb.NewTargetGroup(ctx, "zipkin", &alb.TargetGroupArgs{
+	var err error
+	targetGroup, err = alb.NewTargetGroup(ctx, "zipkin", &alb.TargetGroupArgs{
 		DeregistrationDelay:            pulumi.Int(300),
 		LambdaMultiValueHeadersEnabled: pulumi.Bool(false),
 		Name:                           pulumi.String("sock-Zipki-1I5ZBFCMMEA3L"),
@@ -19,6 +21,44 @@ func newLb(ctx *pulumi.Context) error {
 		SlowStart:                      pulumi.Int(0),
 		TargetType:                     pulumi.String("ip"),
 		VpcId:                          vpc.Vpc.ID(),
+	})
+	if err != nil {
+		return err
+	}
+
+	listener, err := alb.NewListener(ctx, "zipkin", &alb.ListenerArgs{
+		DefaultActions: alb.ListenerDefaultActionArray{
+			&alb.ListenerDefaultActionArgs{
+				TargetGroupArn: targetGroup.ID(),
+				Type:           pulumi.String("forward"),
+			},
+		},
+		LoadBalancerArn: ecs.Lb.Arn,
+		Port:            pulumi.Int(9411),
+		Protocol:        pulumi.String("HTTP"),
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = alb.NewListenerRule(ctx, "zipkin", &alb.ListenerRuleArgs{
+		Actions: &alb.ListenerRuleActionArray{
+			&alb.ListenerRuleActionArgs{
+				TargetGroupArn: targetGroup.Arn,
+				Type:           pulumi.String("forward"),
+			},
+		},
+		Conditions: &alb.ListenerRuleConditionArray{
+			&alb.ListenerRuleConditionArgs{
+				PathPattern: &alb.ListenerRuleConditionPathPatternArgs{
+					Values: pulumi.StringArray{
+						pulumi.String("*"),
+					},
+				},
+			},
+		},
+		ListenerArn: listener.Arn,
+		Priority:    pulumi.Int(1),
 	})
 	if err != nil {
 		return err
